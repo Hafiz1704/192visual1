928e217cc070afd8b648eaccf065975563b88209
hafiz1704 <hafizansyari856@gmail.com>
2020-05-03 02:09:45 +0700

    input dengan berbagai macam komponen

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -5,7 +5,7 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
 
 type
 
@@ -13,9 +13,19 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    btnReset: TButton;
+    cbKota: TComboBox;
+    cbxFoto: TCheckBox;
+    cbxKTP: TCheckBox;
+    cbxIjazah: TCheckBox;
     Edit1: TEdit;
     Memo1: TMemo;
+    rgJenisKelamin: TRadioGroup;
+    procedure btnResetClick(Sender: TObject);
     procedure Button1Click(Sender: TObject);
+    procedure cbxIjazahChange(Sender: TObject);
+    procedure formshow(Sender: TObject);
+    procedure rgJenisKelaminClick(Sender: TObject);
   private
 
   public
@@ -33,7 +43,52 @@ implementation
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-  Memo1.text:=Editl.text;
+  Memo1.lines.Add(Edit1.Text);
+  if(rgJenisKelamin.ItemIndex=0) then
+  begin
+  memo1.lines.add('Laki-Laki');
+  end
+  else  if(rgJenisKelamin.ItemIndex=1)  then
+  begin
+    memo1.lines.add('perempuan');
+  end;
+
+  Memo1.Lines.add(cbkota.Text);
+  if(cbxFoto.Checked) then
+    memo1.Lines.add('Foto ada');
+
+   if(cbxKTP.Checked) then
+    memo1.Lines.add('KTP ada');
+
+   if(cbxIjazah.Checked) then
+    memo1.Lines.add('Ijazah ada');
+end;
+
+procedure TForm1.cbxIjazahChange(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.formshow(Sender: TObject);
+begin
+  btnResetclick(sender);
+end;
+
+procedure TForm1.rgJenisKelaminClick(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.btnResetClick(Sender: TObject);
+begin
+  Edit1.Text:='';
+  Memo1.Clear;
+  rgJenisKelamin.ItemIndex:=-1;
+  cbkota.ItemIndex:=-1;
+  cbkota.Text:='';
+  cbxFoto.Checked:=false;
+  cbxKTP.Checked:=false;
+  cbxIjazah.Checked:=false;
 end;
 
 end.
--- unit1.pas
+++ unit1.pas
@@ -5,17 +5,27 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
-    Button1: TButton;
+    button1: TButton;
+    btnReset: TButton;
+    cbKota: TComboBox;
+    cbxFoto: TCheckBox;
+    cbxKTP: TCheckBox;
+    cbxIjazah: TCheckBox;
     Edit1: TEdit;
     Memo1: TMemo;
-    procedure Button1Click(Sender: TObject);
+    rgJenisKelamin: TRadioGroup;
+    procedure btnResetClick(Sender: TObject);
+    procedure button1Click(Sender: TObject);
+    procedure cbxIjazahChange(Sender: TObject);
+    procedure formshow(Sender: TObject);
+    procedure rgJenisKelaminClick(Sender: TObject);
   private
 
   public
@@ -31,8 +41,54 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.Button1Click(Sender: TObject);
+procedure TForm1.button1Click(Sender: TObject);
 begin
+  Memo1.lines.Add(Edit1.Text);
+  if(rgJenisKelamin.ItemIndex=0) then
+  begin
+  memo1.lines.add('Laki-Laki');
+  end
+  else  if(rgJenisKelamin.ItemIndex=1)  then
+  begin
+    memo1.lines.add('perempuan');
+  end;
+
+  Memo1.Lines.add(cbkota.Text);
+  if(cbxFoto.Checked) then
+    memo1.Lines.add('Foto ada');
+
+   if(cbxKTP.Checked) then
+    memo1.Lines.add('KTP ada');
+
+   if(cbxIjazah.Checked) then
+    memo1.Lines.add('Ijazah ada');
+end;
+
+procedure TForm1.cbxIjazahChange(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.formshow(Sender: TObject);
+begin
+  btnResetclick(sender);
+end;
+
+procedure TForm1.rgJenisKelaminClick(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.btnResetClick(Sender: TObject);
+begin
+  Edit1.Text:='';
+  Memo1.Clear;
+  rgJenisKelamin.ItemIndex:=-1;
+  cbkota.ItemIndex:=-1;
+  cbkota.Text:='';
+  cbxFoto.Checked:=false;
+  cbxKTP.Checked:=false;
+  cbxIjazah.Checked:=false;
 end;
 
 end.
